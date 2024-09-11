cb9a1328c43635c04bc6e49a061b7ecf26dfbba4
HanBo <hanbo9501@gmail.com>
2024-09-11 18:29:38 +0800

    style(part04): remove duplicated code

--- part-04-integrating-spring/spring-17-messaging-in-spring/src/main/java/cloud/popples/messaging/config/AmqpConfig.java
+++ part-04-integrating-spring/spring-17-messaging-in-spring/src/main/java/cloud/popples/messaging/config/AmqpConfig.java
@@ -1,21 +1,16 @@
 package cloud.popples.messaging.config;
 
-import cloud.popples.messaging.listener.AmqpListener;
 import cloud.popples.messaging.service.AmqpService;
 import cloud.popples.messaging.service.AmqpServiceImpl;
-import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.support.converter.JsonMessageConverter;
-import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,10 +29,10 @@ public class AmqpConfig {
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost("113.45.134.208");
+        connectionFactory.setHost("localhost");
         connectionFactory.setPort(5672);
         connectionFactory.setUsername("admin");
-        connectionFactory.setPassword("Hb85701729");
+        connectionFactory.setPassword("admin");
         return connectionFactory;
     }
 
